44066386f0dd167439b6e8ff6cbd655bbc0b52b3
Yulypso <khamphousone.thierry@outlook.fr>
2022-01-17 10:52:28 +0100

    + username

--- src/ServerChat.java
+++ src/ServerChat.java
@@ -1,40 +1,21 @@
 import java.net.*;
 import java.io.*;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ServerChat {
 
     public ServerChat(final int port){
 
         try(ServerSocket listener = new ServerSocket(port)) {
+            while (true) new Thread(new ServiceChat(listener.accept())).start();
 
-            while (true){
-                new Thread(new ServiceChat(listener.accept())).start();
-
-
-                Socket socket = listener.accept();
-
-                // pour envoyer un message aux clients
-                PrintStream out = new PrintStream(socket.getOutputStream());
-
-                // pour lire ce que le client nous envoie
-                InputStreamReader inputStream = new InputStreamReader(socket.getInputStream());
-                BufferedReader input = new BufferedReader(inputStream);
-
-                String message = input.readLine();
-
-                System.out.println(message);
-                String verb = message.split(" ")[0];
-                String res = message.split(" ")[1];
-
-                socket.close();
-            }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-
-    public static void main(String argv[]) throws Exception
+    public static void main(String[] argv)
     {
         new ServerChat(1234);
     }
--- src/ServiceChat.java
+++ src/ServiceChat.java
@@ -1,13 +1,16 @@
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.Scanner;
+import java.util.*;
 
-public class ServiceChat implements Runnable{
+public class ServiceChat implements Runnable {
 
     private Socket socket;
     private Scanner in;
     private PrintWriter out;
+    private String username;
+
+    private static final Map<String, PrintWriter> printWriters = new HashMap<>();
 
     public ServiceChat(Socket socket) {
         this.socket = socket;
@@ -19,12 +22,32 @@ public class ServiceChat implements Runnable{
             this.in = new Scanner(socket.getInputStream());
             this.out = new PrintWriter(socket.getOutputStream(), true);
 
-            if (this.in.hasNextLine()) {
-                String text = this.in.nextLine();
-                this.out.println("text received: " + text);
+            this.out.println("Enter your Username: ");
+            this.username = this.in.nextLine();
+
+            this.out.println("Connected as: " + this.username);
+            this.out.println("-----------------------------");
+
+            printWriters.put(username, this.out);
+
+            while(true) {
+                if (this.in.hasNextLine()) {
+                    String text = this.in.nextLine();
+
+                    for(Map.Entry<String, PrintWriter> pw : printWriters.entrySet()) {
+                        System.out.println("[" + this.username + "] " + text);
+                        pw.getValue().println("[" + this.username + "] " + text);
+                    }
+
+                    if (text.equals("exit")) {
+                        this.socket.close();
+                        return;
+                    }
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 }
+
