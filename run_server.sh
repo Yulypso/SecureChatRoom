fb4103aa5bd48ba3d6c17a28a5f72eed05912109
Yulypso <khamphousone.thierry@outlook.fr>
2022-01-21 23:36:04 +0100

    + run_server.sh

---
+++ run_server.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+javac --enable-preview --release 17 src/Server/*.java src/Server/Models/*.java src/Server/Utils/*.java -d prod
+java --enable-preview -cp out Server/ServerChat
