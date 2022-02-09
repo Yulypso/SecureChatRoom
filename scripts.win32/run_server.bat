00f5e8b43007159c9bc8a482d93393b61cf629a3
Yulypso <khamphousone.thierry@outlook.fr>
2022-02-09 16:58:45 +0100

    save

---
+++ scripts.win32/run_server.bat
@@ -0,0 +1,3 @@
+javac --enable-preview --release 17 ../src/secureApp/server/*.java ../src/secureApp/server/Models/*.java ../src/secureApp/server/Utils/*.java -d ../out
+java --enable-preview -cp ../out secureApp/server/ServerChat
+pause
